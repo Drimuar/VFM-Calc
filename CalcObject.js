ecf5076091c5ef790a824c03bfdfd61205520d17
Drimuar <Drimuar@gmail.com>
2022-02-07 15:47:21 +0700

    switch to object :)

--- CalcObject.js
+++ CalcObject.js
@@ -9,29 +9,25 @@ function Calc(operation, a, b) {
 	b = +b;
 	const isNotValid = !a && a !== 0 || !b && b !== 0 || !operation;
 	if (isNotValid) return "Error";
-	switch (operation) {
-		case "sum":
-			return a + b;
-		case "sub":
-			return a - b;
-		case "multi":
-			return a * b;
-		case "div":
-			return a / b;
-		case "pow":
-			return a ** b;
-		case "mod":
-			return a % b;
-		default:
-			return "Unknown operation";
+
+	let operations = {
+		sum: a + b,
+		sub: a - b,
+		multi: a * b,
+		div: a / b,
+		pow: a ** b,
+		mod: a % b
 	}
+	return operations[operation];
 }
 
 console.log(Calc("sum", 5, 5));
+console.log(Calc("sub", 5, 15));
 console.log(Calc("div", 10, 3));
 console.log(Calc("pow", 2, 5));
 console.log(Calc("mod", 10, 3));
 console.log(Calc("sum", 5, "abc"));
+
 
 
 // alert(Calc(operation, a, b));
